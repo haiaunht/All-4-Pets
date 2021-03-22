c00ca092b6e88856d56e70553e41d794a550c922
Hai-Au Bui <haiaunht@yahoo.com>
2021-03-22 09:04:10 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Authors: Isabel Servin, Ryne Rountree, Sang Vo, Hai-Au Bui
+
+- Our app name is : All-4-Pets
+- Our database name is : PokeDex
---
+++ client/components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { hot } from "react-hot-loader/root"
+
+import { Route, Switch, BrowserRouter } from "react-router-dom"
+
+const App = props => {
+  return <div>Replace this div with your Router.</div>
+}
+
+export default hot(App)
