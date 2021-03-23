8260af5463aef2690576e21ae6ac8baa07e399da
yauacom <70412615+yauacom@users.noreply.github.com>
2021-03-23 08:29:29 -0700

    Add Navbar components in client components

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,7 +1,31 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
-  return <div>This is a navbar</div>
+  return (
+    <div className="navbar">
+      <nav>
+        <h1 className="brand">A.R.I.S. PROJECT</h1>
+        <ul>
+          <li>
+            <Link to="/pets">Home</Link>
+          </li>
+          <li>
+            <Link to="/pets/puppies">Puppies</Link>
+          </li>
+          <li>
+            <Link to="/pets/pokemon">Pokemon</Link>
+          </li>
+          <li>
+            <Link to="/adoptions/new">Add a Pet</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default Navbar
