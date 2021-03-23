4941b56378615efd9e0b44bc4531ae235cf976b5
yauacom <70412615+yauacom@users.noreply.github.com>
2021-03-23 06:57:48 -0700

    Modify the App components of client side

---
+++ client/components/PuppiesIndexPage.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const PuppiesIndexPage = () => {
+  return <div>This is a PuppiesIndexPage.</div>
+}
+
+export default PuppiesIndexPage
---
+++ client/components/PuppiesShowPage.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const PuppiesShowPage = () => {
+  return <div>This is a puppies show page</div>
+}
+
+export default PuppiesShowPage
